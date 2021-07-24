9c0b15037254e462999fca1cf8d6985b024b539d
Tayanne <tayh.snovaes@gmail.com>
2021-07-24 19:33:09 -0300

    add services

---
+++ services/service.js
@@ -0,0 +1,15 @@
+import repository from '../repositories/repository.js'
+
+async function getSaldo(conta) {
+    return await repository.getSaldo(conta);
+}
+
+async function sacar(conta, valor) {
+    return await repository.sacar(conta, valor);
+}
+
+async function depositar(conta, valor) {
+    return await repository.depositar(conta, valor);
+}
+
+export default { getSaldo, sacar, depositar }
