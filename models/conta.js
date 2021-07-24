4169049979ccaff1af197bb9dacf2fea0ff67fe1
Tayanne <tayh.snovaes@gmail.com>
2021-07-24 19:32:22 -0300

    add models/conta

---
+++ models/conta.js
@@ -0,0 +1,27 @@
+import { sequelize } from '../database.js';
+import Sequelize from 'sequelize';
+
+// cria a tabela
+export const Contas = sequelize.define('contas', {
+	nome: {
+		type: Sequelize.STRING,
+	},
+	conta: {
+		type: Sequelize.INTEGER,
+	},
+	saldo: {
+		type: Sequelize.DECIMAL,
+	},
+});
+
+
+
+//força a criação caso ainda não exista
+// Contas.sync({ force: true });
+
+//popula a tabela
+// Contas.create({
+// 	nome: 'Adilson',
+// 	conta: 789,
+// 	saldo: 5000.0,
+// });
